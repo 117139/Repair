4894c8d29e0f263a922dea7e1e94e58aff0b5097
wenxin <1171390035@qq.com>
2019-09-02 18:54:05 +0800

    9-2
    9-2

--- pages/fuwu/fuwu.js
+++ pages/fuwu/fuwu.js
@@ -6,47 +6,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-		mdd_data:[
-		 {
-			 pic:'../../static/images/index_11.png',
-			 name:'欧洲'
-		 },
-		 {
-			 pic:'../../static/images/index_13.png',
-			 name:'美洲'
-		 },
-		 {
-			 pic:'../../static/images/index_15.png',
-			 name:'中东非洲'
-		 },
-		 {
-			 pic:'../../static/images/index_17.png',
-			 name:'大洋洲'
-		 },
-		 {
-			 pic:'../../static/images/index_19.png',
-			 name:'东南亚'
-		 },
-		 {
-			 pic:'../../static/images/index_30.png',
-			 name:'日韩'
-		 },
-		 {
-			 pic:'../../static/images/index_32.png',
-			 name:'极地'
-		 },
-		 {
-			 pic:'../../static/images/index_33.png',
-			 name:'海岛'
-		 },
-		 {
-			 pic:'../../static/images/index_26.png',
-			 name:'国内'
-		 },
-		 // {
-			//  pic:'../../static/images/index_27.png',
-			//  name:'更多'
-		 // },
+		fw_data: [{
+				pic: '../../static/images/index_07.png',
+				name: '管道疏通'
+			},
+			{
+				pic: '../../static/images/index_09.png',
+				name: '家电维修'
+			},
+			{
+				pic: '../../static/images/index_11.png',
+				name: '灯具电路'
+			},
+			{
+				pic: '../../static/images/index_13.png',
+				name: '卫浴洁具'
+			},
+			{
+				pic: '../../static/images/index_19.png',
+				name: '门窗维修'
+			},
+			{
+				pic: '../../static/images/index_20.png',
+				name: '门锁开换'
+			},
+			{
+				pic: '../../static/images/index_21.png',
+				name: '家具安装'
+			},
+			{
+				pic: '../../static/images/index_22.png',
+				name: '家电清洗'
+			},
 		],
 		dest:[
 		 {
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,73 +3,95 @@
 const app = getApp()
 
 Page({
-  data: {
-   bannerimg:[
-		 {
-		 pic:'/static/images/banner_03.png'
-		},
-		 {
-		 pic:'/static/images/banner_03.png'
-		},
-		 {
-		 pic:'/static/images/banner_03.png'
-		},
-	 ],
-	 mdd_data:[
-		 {
-			 pic:'../../static/images/index_11.png',
-			 name:'欧洲'
-		 },
-		 {
-			 pic:'../../static/images/index_13.png',
-			 name:'美洲'
-		 },
-		 {
-			 pic:'../../static/images/index_15.png',
-			 name:'中东非洲'
-		 },
-		 {
-			 pic:'../../static/images/index_17.png',
-			 name:'大洋洲'
-		 },
-		 {
-			 pic:'../../static/images/index_19.png',
-			 name:'东南亚'
-		 },
-		 {
-			 pic:'../../static/images/index_30.png',
-			 name:'日韩'
-		 },
-		 {
-			 pic:'../../static/images/index_32.png',
-			 name:'极地'
-		 },
-		 {
-			 pic:'../../static/images/index_33.png',
-			 name:'海岛'
-		 },
-		 {
-			 pic:'../../static/images/index_26.png',
-			 name:'国内'
-		 },
-		 {
-			 pic:'../../static/images/index_27.png',
-			 name:'更多'
-		 },
-	 ],
-	  indicatorDots: true,
-	 autoplay: true,
-	 interval: 3000,
-	 duration: 1000,
-	 circular:true
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    
-  },
+	data: {
+		bannerimg: [{
+				pic: '/static/images/index_03.png'
+			},
+			{
+				pic: '/static/images/index_03.png'
+			},
+			{
+				pic: '/static/images/index_03.png'
+			},
+		],
+		fw_data: [{
+				pic: '../../static/images/index_07.png',
+				name: '管道疏通'
+			},
+			{
+				pic: '../../static/images/index_09.png',
+				name: '家电维修'
+			},
+			{
+				pic: '../../static/images/index_11.png',
+				name: '灯具电路'
+			},
+			{
+				pic: '../../static/images/index_13.png',
+				name: '卫浴洁具'
+			},
+			{
+				pic: '../../static/images/index_19.png',
+				name: '门窗维修'
+			},
+			{
+				pic: '../../static/images/index_20.png',
+				name: '门锁开换'
+			},
+			{
+				pic: '../../static/images/index_21.png',
+				name: '家具安装'
+			},
+			{
+				pic: '../../static/images/index_22.png',
+				name: '家电清洗'
+			},
+		],
+
+		hot_data: [{
+				pic: '../../static/images/index_28.jpg',
+				name: '水龙头维修'
+			},
+			{
+				pic: '../../static/images/index_31.jpg',
+				name: '空调维修'
+			},
+			{
+				pic: '../../static/images/index_33.jpg',
+				name: '家电维修'
+			},
+		],
+		cai_data:[
+			{
+					pic: '../../static/images/index_42.jpg',
+					name: '水龙头维修'
+				},
+				{
+					pic: '../../static/images/index_43.jpg',
+					name: '空调维修'
+				},
+				{
+					pic: '../../static/images/index_47.jpg',
+					name: '家电维修'
+				},
+				{
+					pic: '../../static/images/index_48.jpg',
+					name: '家电维修'
+				},
+		],
+		indicatorDots: true,
+		autoplay: true,
+		interval: 3000,
+		duration: 1000,
+		circular: true
+	},
+	//事件处理函数
+	bindViewTap: function() {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	onLoad: function() {
+
+	},
 })
